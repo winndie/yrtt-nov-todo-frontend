424917e2eb841dfcd0591ed225c9a5a13218ece2
Winndie Fan <winndie@gmail.com>
2021-03-25 08:50:38 +0000

    set completed tasks header to 'done'

--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -4,7 +4,7 @@ import './TaskList.css';
 function TaskList({ tasks, status, deleteTask }) {
   return (
     <section className={`${status}-tasks`}>
-        <h2 className="heading">Tasks to do:</h2>
+        <h2 className="heading">Tasks {status==='complete'?'done':'to do'}:</h2>
         <ul className="task-list">
             { tasks.map(task => 
               <Task 
